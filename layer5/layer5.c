c8b30026101290ae4f0a0058a20ad971f48d7323
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-08-25 21:02:50 +0530

    Add PING Application

---
+++ layer5/layer5.c
@@ -0,0 +1,18 @@
+//
+//  layer5.c
+//  tcp_ip_stack_c
+//
+//  Created by Yash soni on 20/08/23.
+//
+
+#include <stdio.h>
+#include "graph.h"
+
+void promote_pkt_to_layer5(node_t *node,
+                           interface_t *recv_intf,
+                           char *l5_hdr,
+                           unsigned int pkt_size,
+                           int l5_protocol_number)
+{
+    
+}
---
+++ layer5/ping.c
@@ -0,0 +1,23 @@
+//
+//  ping.c
+//  tcp_ip_stack_c
+//
+//  Created by Yash soni on 20/08/23.
+//
+
+#include <stdio.h>
+#include "../graph.h"
+#include <arpa/inet.h>
+
+
+void
+layer5_ping_fn(node_t *node, char *dst_ip_addr)
+{
+    unsigned int addr_int;
+    
+    printf("Src node : %s, Ping ip : %s\n", node->name, dst_ip_addr);
+    
+    inet_pton(AF_INET, dst_ip_addr, &addr_int);
+    
+    addr_int = htonl(addr_int);
+}
