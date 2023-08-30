35d3dabb5911390cb95befa82e7780528398daac
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-08-30 18:50:02 +0530

    Comm Error Fix

--- comm.c
+++ comm.c
@@ -7,73 +7,92 @@
 #include "comm.h"
 #include "graph.h"
 #include "net.h"
+#include <errno.h>
 
 
-static int _send_pkt_out(int socket, char *data, unsigned int size,
+static int
+_send_pkt_out(int sock_fd, char *pkt_data, unsigned int pkt_size,
                          unsigned int dst_udp_port_no)
 {
-    int sent_recv_bytes = 0;
-    struct sockaddr_in dest;
-    struct hostent *host = (struct hostent *) gethostbyname("127.0.0.1");
-    dest.sin_family = AF_INET;
-    dest.sin_port = dst_udp_port_no;
-    dest.sin_addr = *((struct in_addr *)host->h_addr);
+    printf("\nIn the function %s\n", __FUNCTION__);
+
+    int rc = 0;
+    struct sockaddr_in dest_addr;
     
-    sent_recv_bytes = (int)sendto(socket, (char *)data,
-                                  size, 0, (struct sockaddr *)&dest,
-                                  sizeof(struct sockaddr));
-    if(sent_recv_bytes == -1) {
+    struct hostent *host = (struct hostent *) gethostbyname("127.0.0.1");
+    
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = dst_udp_port_no;
+    dest_addr.sin_addr = *((struct in_addr *)host->h_addr);
+    
+    rc = sendto(sock_fd, pkt_data,
+                pkt_size, 0, (struct sockaddr *)&dest_addr,
+                sizeof(struct sockaddr));
+    
+    
+    if(rc == -1) {
         perror("Couldn't send the data to udp port\n");
     }
-    return sent_recv_bytes;
+    return rc;
 }
 
 static unsigned int udp_port_number = 40000;
-static unsigned int get_next_port_number() {
+
+static unsigned int
+get_next_udp_port_number()
+{
     return udp_port_number++;
 }
 
-void init_udp_socket(node_t *node)
+void
+init_udp_socket(node_t *node)
 {
-    node->udp_port_number = get_next_port_number();
-    int udp_sock_fd = {0};
-    udp_sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(udp_sock_fd == -1)
-    {
-        printf("Failed to create the socket\n");
-        exit(EXIT_FAILURE);
+    printf("\nIn the function %s\n", __FUNCTION__);
+
+    if(node->udp_port_number) return;
+
+    node->udp_port_number = get_next_udp_port_number();
+    
+    int udp_sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if(udp_sock_fd == -1) {
+        printf("Socked creation failed for node %s\n", node->name);
+        return;
     }
     
-    struct sockaddr_in in_node_addr;
-    in_node_addr.sin_family = AF_INET;
-    in_node_addr.sin_port = node->udp_port_number;
-    in_node_addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in node_addr;
+    node_addr.sin_family = AF_INET;
+    node_addr.sin_port = node->udp_port_number;
+    node_addr.sin_addr.s_addr = INADDR_ANY;
     
-    int b = bind(udp_sock_fd,
-                 (struct sockaddr *)&in_node_addr,
-                 sizeof(struct sockaddr));
-    if(b == -1)
-    {
-        printf("Failed to bind the socket for node %s\n",
+    
+    if(bind(udp_sock_fd,
+            (struct sockaddr *)&node_addr,
+            sizeof(struct sockaddr)) == -1) {
+        printf("Error : failed to bind the socket for node %s\n",
                node->name);
-        exit(EXIT_FAILURE);
+        return;
     }
+    
     node->udp_sock_fd = udp_sock_fd;
 }
 
 static char recv_buffer[MAX_PACKET_BUFFER_SIZE];
 static char send_buffer[MAX_PACKET_BUFFER_SIZE];
 
-static void _pkt_receive(node_t *receiving_node,
-                         char *pkt_with_aux_data,
-                         unsigned int pkt_size)
+static void
+_pkt_receive(node_t *receiving_node,
+             char *pkt_with_aux_data,
+             unsigned int pkt_size)
 {
+    printf("\nIn the function %s\n", __FUNCTION__);
+
     char *recv_intf_name = pkt_with_aux_data;
     interface_t *recv_intf = get_node_if_by_name(receiving_node,
                                                  recv_intf_name);
     if(!recv_intf) {
-        printf("Pkt received on unknown interface %s\n",
-               recv_intf->if_name);
+        printf("Error : pkt received on unknown interface %s on node %s\n",
+               recv_intf->if_name, receiving_node->name);
         return;
     }
     
@@ -85,11 +104,13 @@ static void _pkt_receive(node_t *receiving_node,
 static void *
 _network_start_pkt_receiver_thread(void *arg)
 {
+    printf("\nIn the function %s\n", __FUNCTION__);
+
     node_t *node;
     glthread_t *curr;
     
     fd_set active_sock_fd_set,
-        backup_sock_fd_set;
+           backup_sock_fd_set;
     
     int sock_max_fd = 0,
         bytes_recvd = 0;
@@ -104,29 +125,36 @@ _network_start_pkt_receiver_thread(void *arg)
     struct sockaddr_in sender_addr;
     
     ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr) {
+        
         node = graph_glue_to_node(curr);
-        if(!node->udp_sock_fd)
-            continue;
+        
+        if(!node->udp_sock_fd)  continue;
+            
         if(node->udp_sock_fd > sock_max_fd)
             sock_max_fd = node->udp_sock_fd;
         
         FD_SET(node->udp_sock_fd, &backup_sock_fd_set);
+        
     } ITERATE_GLTHREAD_END(&topo->node_list, curr);
     
-    while (1) {
-        memcpy(&active_sock_fd_set, &active_sock_fd_set, sizeof(fd_set));
+    while(1) {
+        memcpy(&active_sock_fd_set, &backup_sock_fd_set, sizeof(fd_set));
         select(sock_max_fd + 1, &active_sock_fd_set, NULL, NULL, NULL);
         
         ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr) {
+            
             node = graph_glue_to_node(curr);
             
             if(FD_ISSET(node->udp_sock_fd, &active_sock_fd_set)) {
+                
                 memset(recv_buffer, 0, MAX_PACKET_BUFFER_SIZE);
                 bytes_recvd = (int)recvfrom(node->udp_sock_fd,
                                             (char *)recv_buffer, MAX_PACKET_BUFFER_SIZE,
                                             0, (struct sockaddr *)&sender_addr,
                                             (unsigned int *)&addr_len);
+                
                 _pkt_receive(node, recv_buffer, bytes_recvd);
+                
             }
         } ITERATE_GLTHREAD_END(&topo->node_list, curr);
     }
@@ -135,6 +163,8 @@ _network_start_pkt_receiver_thread(void *arg)
 void
 network_start_pkt_receiver_thread(graph_t *topo)
 {
+    printf("\nIn the function %s\n", __FUNCTION__);
+
     pthread_attr_t attr;
     pthread_t recv_pkt_thread;
     
@@ -148,6 +178,8 @@ network_start_pkt_receiver_thread(graph_t *topo)
 int send_pkt_out(char *pkt, unsigned int pkt_size,
                  interface_t *interface)
 {
+    printf("\nIn the function %s\n", __FUNCTION__);
+
     int rc = 0;
     node_t *sending_node = interface->att_node;
     node_t *nbr_node = get_nbr_node(interface);
@@ -165,12 +197,12 @@ int send_pkt_out(char *pkt, unsigned int pkt_size,
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
     if(sock < 0) {
-        perror("Sending socket creation failed\n");
+        printf("Error : Sending socket Creation failed, errno = %d\n", errno);
         return -1;
     }
     
-    interface_t *other_interface = &interface->link->intf1 == interface?
-                                    &interface->link->intf2:&interface->link->intf1;
+    interface_t *other_interface = &interface->link->intf1 == interface ? \
+                                    &interface->link->intf2 : &interface->link->intf1;
     
     memset(send_buffer, 0, MAX_PACKET_BUFFER_SIZE);
     
@@ -178,7 +210,9 @@ int send_pkt_out(char *pkt, unsigned int pkt_size,
     
     strncpy(pkt_with_aux_data, other_interface->if_name,
             IF_NAME_SIZE);
+    
     pkt_with_aux_data[IF_NAME_SIZE - 1] = '\0';
+    
     memcpy(pkt_with_aux_data + IF_NAME_SIZE, pkt, pkt_size);
     
     rc = _send_pkt_out(sock, pkt_with_aux_data, pkt_size + IF_NAME_SIZE,
@@ -191,11 +225,16 @@ extern void
 layer2_frame_recv(node_t *node, interface_t *interface,
                   char *pkt, unsigned int pkt_size);
 
-int pkt_receive(node_t *node,
+int
+pkt_receive(node_t *node,
                  interface_t *interface,
                  char *pkt,
                  unsigned int pkt_size)
 {
+    printf("\n\nmsg recvd = %s, on node = %s, iif = %s\n", pkt, node->name, interface->if_name);
+    
+    return 0;
+    
     pkt = pkt_buffer_shift_right(pkt, pkt_size,
                                  MAX_PACKET_BUFFER_SIZE - IF_NAME_SIZE);
     layer2_frame_recv(node, interface, pkt, pkt_size);
@@ -206,15 +245,16 @@ int send_pkt_flood(node_t *node,
                    interface_t *exempted_intf,
                    char *pkt, unsigned int pkt_size)
 {
-    interface_t *curr_intf;
+    interface_t *intf;
     
     for(int i=0; i < MAX_INTF_PER_NODE; ++i)
     {
-        curr_intf = node->interfaces[i];
-        if(!curr_intf) return 0;
-        if(curr_intf == exempted_intf)
-            continue;
-        send_pkt_out(pkt, pkt_size, curr_intf);
+        intf = node->interfaces[i];
+        if(!intf) return 0;
+        
+        if(intf == exempted_intf) continue;
+            
+        send_pkt_out(pkt, pkt_size, intf);
     }
     return 0;
 }
