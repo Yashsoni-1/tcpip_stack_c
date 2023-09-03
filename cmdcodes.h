57bbbbcc4577e25d234bc6e7e32df49e8422ce19
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-09-03 17:41:20 +0530

    Update cmdcodes.h

--- cmdcodes.h
+++ cmdcodes.h
@@ -1,9 +1,9 @@
 #define CMDCODE_SHOW_NW_TOPOLOGY       1
-#define CMDCODE_PING                   2
+#define CMDCODE_PING                   2              
 #define CMDCODE_SHOW_NODE_ARP_TABLE    3
 #define CMDCODE_RUN_ARP                4
 #define CMDCODE_SHOW_NODE_MAC_TABLE    8
 #define CMDCODE_SHOW_NODE_RT_TABLE     9
 #define CMDCODE_CONF_NODE_L3ROUTE     10
-
+#define CMDCODE_ERO_PING              11
 
