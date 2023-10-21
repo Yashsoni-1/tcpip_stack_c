ec0875e28f151c4b53a4c4e0c3ad3936d0763d28
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-21 20:14:44 +0530

    Update tcp_public.h

--- tcp_public.h
+++ tcp_public.h
@@ -1,12 +1,23 @@
-//
-//  tcp_public.h
-//  tcp_ip_stack_c
-//
-//  Created by Yash soni on 05/09/23.
-//
-
 #ifndef tcp_public_h
 #define tcp_public_h
+
+#include <assert.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include "tcpconst.h"
+#include "graph.h"
+#include "net.h"
+#include "Layer2/layer2.h"
+#include "Layer3/layer3.h"
+#include "utils.h"
+#include "comm.h"
+#include "gl_thread/gl_thread.h"
+#include "CommandParser/libcli.h"
+#include "CommandParser/cmdtlv.h"
+#include "cmdcodes.h"
+
+
+extern graph_t *topo;
 
 
 #endif /* tcp_public_h */
