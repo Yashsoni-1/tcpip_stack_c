d7c3e34a724a6baeea937ec78660cea1cab7df6d
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-22 09:44:23 +0530

    Update utils.c

--- utils.c
+++ utils.c
@@ -34,4 +34,30 @@ void layer2_fill_with_broadcast_mac(unsigned char *mac_array)
     mac_array[5] = 0xff;
 }
 
+unsigned int
+ip_p_to_n(char *ip_addr)
+{
+    uint32_t ip = 0;
+    inet_pton(AF_INET, ip_addr, &ip);
+    ip = htonl(ip);
+    return ip;
+}
+
+char *
+ip_n_to_p(unsigned int ip_addr, char *output_buffer)
+{   
+    char *out = NULL;
+    
+    static char str_ip[16];
+    
+    out = !output_buffer ? str_ip : output_buffer;
+    
+    memset(out, 0, 16);
+
+    ip_addr = htonl(ip_addr);
+    inet_ntop(AF_INET, &ip_addr, out, 16);
+    out[15] = '\0';
+    
+    return out;
+}
 
