d82c605b96da2a962fb8a9b0b45c4f9114e71a7c
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-22 09:45:07 +0530

    Update net.c

--- net.c
+++ net.c
@@ -120,32 +120,6 @@ node_get_matching_subnet_interface(node_t *node, char *ip_addr)
     return NULL;
 }
 
-unsigned int
-ip_p_to_n(char *ip_addr)
-{
-    uint32_t ip = 0;
-    inet_pton(AF_INET, ip_addr, &ip);
-    ip = htonl(ip);
-    return ip;
-}
-
-char *
-ip_n_to_p(unsigned int ip_addr, char *output_buffer)
-{   
-    char *out = NULL;
-    
-    static char str_ip[16];
-    
-    out = !output_buffer ? str_ip : output_buffer;
-    
-    memset(out, 0, 16);
-
-    ip_addr = htonl(ip_addr);
-    inet_ntop(AF_INET, &ip_addr, out, 16);
-    out[15] = '\0';
-    
-    return out;
-}
 
 char *
 pkt_buffer_shift_right(char *pkt, unsigned int pkt_size,
