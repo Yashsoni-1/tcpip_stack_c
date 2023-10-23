cdd002415460ec3155bacf4e865e54ee91b1ae7e
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-23 14:46:58 +0530

    Create app_handlers.h

---
+++ layer5/app_handlers.h
@@ -0,0 +1,11 @@
+#ifndef __app_handlers__
+#define __app_handlers__
+
+#include "../CommandParser/libcli.h"
+
+int 
+spf_algo_handler(param_t *param, ser_buff_t *tlv_buff, op_mode enable_or_disable);
+
+
+
+#endif 
