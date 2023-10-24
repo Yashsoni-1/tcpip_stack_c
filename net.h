a2bffa1f20f275f8c98381598c1adac1cb2c4a4e
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-24 17:03:44 +0530

    Update net.h

--- net.h
+++ net.h
@@ -170,4 +170,20 @@ void print_mac(char *heading, unsigned char *mac_address);
 
 bool_t is_interface_l3_bidirectional(interface_t *interface);
 
+#define ITERATE_NODE_NBRS_BEGIN(node_ptr, nbr_ptr, oif_ptr, ip_addr_ptr) \
+    do{ 																\
+			int i=0;													\
+			interface_t *other_intf;									\
+																		\						
+			for(i=0; i < MAX_INTF_PER_NODE; ++i) {						\
+				oif_ptr = node_ptr[i];									\
+				if(!oif_ptr) continue;									\
+				other_intf = &oif_ptr->link->intf1 == oif_ptr ? 		\
+					&oif_ptr->link->intf2 : &oif_ptr->link->intf1;		\
+				if(!other_intf) continue;								\
+				nbr_ptr = get_nbr_node(oif_ptr);						\
+				ip_addr_ptr = IF_IP(other_intf);						\
+
+#define ITERATE_NODE_NBRS_END(node_ptr, nbr_ptr, oif_ptr, ip_addr_ptr) }}while(0);
+
 #endif /* net_h */
