42d04a602043e265606312e69bbe1bdd21821c6c
Yashsoni-1 <113181346+Yashsoni-1@users.noreply.github.com>
2023-10-24 10:15:05 +0530

    Update spf.c

--- layer5/spf_algo/spf.c
+++ layer5/spf_algo/spf.c
@@ -4,6 +4,10 @@
 
 extern graph_t *topo;
 
+#define INFINITE_METRIC 0xFFFFFFFF
+
+#define SPF_METRIC(nodeptr) (nodeptr->spf_data->spf_metric)
+
 void compute_spf(node_t *spf_root)
 {
 	printf("%s called...\n", __FUNCTION__);
@@ -55,6 +59,111 @@ void spf_flush_nexthops(nexthop_t **nexthop)
 			
 		}
 	}
+}
+
+static inline void 
+free_spf_result(spf_result_t *spf_result) 
+{
+	spf_flush_nexthops(spf_result->nexthops);
+	remove_glthread(&spf_result->spf_res_glue);
+	free(spf_result);
+}
+
+static nexthop_t *
+create_new_nexthop(interface_t *oif)
+{
+	nexthop_t *nexthop = calloc(1, sizeof(nexthop_t));
+	nexthop->oif = oif;
+	interface_t *other_intf = &oif->link->intf1 == oif ? &oif->link->intf2 : &oif->link->intf1;
+	if(!other_intf) {
+		free(nexthop);
+		return NULL;
+	}
+
+	strncpy(nexthop->gw_ip, IF_IP(other_intf), 16);
+	nexthop->ref_count = 0;
+	return nexthop;
+}
+
+static bool_t
+spf_insert_new_nexthop(nexthop_t **nexthop_array, nexthop_t *nexthop)
+{
+	int i=0;
+
+	for(; i < MAX_NXT_HOPS; ++i) {
+		if(nexthop_array[i]) continue;
+		nexthop[i] = nexthop;
+		nexthop[i]->ref_count++;
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+static bool_t
+spf_is_nexthop_exist(nexthop_t **nexthop_array, nexthop_t *nexthop)
+{
+	int i=0;
+
+	for(; i < MAX_NXT_HOPS; ++i) {
+		if(!nexthop_array[i]) continue;
+		
+		if(nexthop_array[i]->oif == nexthop->oif) return TRUE;
+	}
+
+	return FALSE;
+}
+
+static int 
+spf_union_nexthops_array(nexthop_t **src, nexthop_t **dst)
+{
+	int i=0, j=0, copied_count=0;
+
+	while(j < MAX_NXT_HOPS && dst[j]) j++;
+
+	if(j == MAX_NXT_HOPS) return 0;
+
+	for(; i < MAX_NXT_HOPS && j < MAX_NXT_HOPS; ++i, ++j) {
+
+		if(src[i] && spf_is_nexthop_exist(dst, src[i]) == FALSE) {
+			dst[j] = src[i];
+			dst[j]->ref_count++;
+			copied_count++;
+		}
+	}
+
+	return copied_count;
+}
+
+static int
+spf_comparison_fn(void *data1, void *data2)
+{
+	spf_data_t *spf_data_1 = (spf_data_t *)data1;
+	spf_data_t *spf_data_2 = (spf_data_t *)data2;
+
+	if(spf_data_1->spf_metric < spf_data_2->spf_metric) 
+		return -1;
+	else if(spf_data_1->spf_metric > spf_data_2->spf_metric) 
+		return 1;
+
+	return 0;
+}
+
+static spf_result_t *
+spf_lookup_spf_result_by_node(node_t *spf_root, node_t *node)
+{
+	glthread_t *curr;
+	spf_result_t *spf_result;
+	spf_data_t *curr_spf_data;
+
+	ITERATE_GLTHREAD_BEGIN(&spf_root->spf_data->spf_result_head, curr) {
+		spr_result = spf_res_glue_to_spf_result(curr);
+		if(spf_result->node == node) {
+			return spf_result;
+		}
+	} ITERATE_GLTHREAD_END(&spf_root->spf_data->spf_result_head, curr) 
+
+	return NULL:
 }
 
 int 
